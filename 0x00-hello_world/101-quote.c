bec1ea19fae5587e0b4813b5f7aa6e78bf88e962
Onotu Umoru <onotu128@gmail.com>
2022-04-11 21:32:23 +0100

    new commit

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: 1 (Failure)
+*/
+int main(void)
+{
+
+	fwrite(stderr, "and that piece of art is useful\" -Dora Korpar, 2015-10-19\n");
+	return (1);
+
+}
