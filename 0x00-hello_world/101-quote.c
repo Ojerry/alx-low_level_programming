a6bce7918b1f798e03348083785056d4dcb660e4
Onotu Umoru <onotu128@gmail.com>
2022-04-11 21:52:24 +0100

    new commit

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,14 +1,16 @@
-#include <stdio.h>
+#include<unistd.h>
 
 /**
  * main - Entry point
  *
- * Return: 1 (Failure)
+ * Description: print a quote using write function
+ * Return: This time we return an error 1.
 */
+
 int main(void)
 {
+	char quo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
-	fwrite(stderr, "and that piece of art is useful\" -Dora Korpar, 2015-10-19\n");
+	write(1, quo, 59);
 	return (1);
-
 }
