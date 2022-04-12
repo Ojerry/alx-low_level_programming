cd6968e9d44dbf17f68353ed15a83676f1520500
Onotu Umoru <onotu128@gmail.com>
2022-04-12 16:55:07 +0100

    new commit

---
+++ 0x02-functions_nested-loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - This function prints the alphabet 10 times
+ * in lowercase, followed by a newline
+ */
+void print_alphabet_x10(void)
+{
+	int i = 0;
+
+	while (i < 10)
+	{
+		char c;
+
+		for (c = 'a'; c <= 'z'; c++)
+		{
+			_putchar(c);
+		}
+
+		_putchar('\n');
+		i++;
+	}
+}
