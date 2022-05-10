c6b0517ce26ed288664e3d4dcd6cbaeef15de719
Ojerry <onotu128@gmail.com>
2022-05-10 05:54:12 -0700

    new commit

---
+++ 0x09-static_libraries/3-islower.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * _islower - checks whether the passed character is a lowercase letter
+ * @c: The character to check
+ *
+ * Return: 1 if c is lowercase, otherwise Return: 0
+ */
+int _islower(int c)
+{
+	int i;
+
+	i = c;
+
+	if (i >= 97 && i <= 122)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
