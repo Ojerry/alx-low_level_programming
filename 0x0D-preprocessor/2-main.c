cb32bc296204718ace0eff7c05643d8717f4cff8
Ojerry <onotu128@gmail.com>
2022-05-16 09:23:35 -0700

    made changes

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
 
-/*
-* main - function to print name of program
-* Return - return 0 if successful
+/**
+* main - entry point
+*
+* Description: print the name of the source file
+*
+* Return - always 0 if success
 */
 
 int main(void)
