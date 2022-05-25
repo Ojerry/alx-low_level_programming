0dc582662884f03d2cbbb12911939e910027ae07
Ojerry <onotu128@gmail.com>
2022-05-25 06:38:29 -0700

    update

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -2,15 +2,13 @@
 
 /**
 *print_name - prints a name
-*@name:name to be printed
-*@f: function which print name
+*@name: character string
+*@f: function to print name 
 */
 
 void print_name(char *name, (*f)(char *))
 {
 
-if (name == NULL || f = NULL)
-return;
-
+if (name != NULL && f != NULL)
 f(name);
 }
